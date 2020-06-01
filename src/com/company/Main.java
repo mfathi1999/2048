cadb6913b7ae8396e6fef2362bf9cdca12685091
Javad <mail@heidarpoor.ir>
2020-06-01 23:43:48 +0430

    graphical text added 23:43 :)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 
-//import Veiw
 import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
@@ -9,15 +8,42 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static void main(String[] args) {
+//    private static void welcome()
+//    {
+//        String welcome = "\n\n\nWelcome to 2048 Game ;) \nChoose the Arrow to move on blocks !\nYou can use [w for up, a for left, d for right, s for down .";
+//        System.out.println(welcome);
+//    }
+
+    private static int handle(char arrow)
+    {
+        if(arrow =='w')
+            return 0;
+        else if(arrow == 's')
+            return 1;
+        else if(arrow == 'a')
+            return 3;
+        else if(arrow == 'd')
+            return 2;
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         View view = new View();
         view.startGame();
+//        System.out.println("__   _____  _   _  __        _____ _   _ \n\\ \\ / / / / _ \\ \\| | | |    / /|_ _|\\ \\ |\n \\ V / | | | | | |  \\ \\ /\\ / / | ||  \\| |\n  | || |_| | |_| |   \\ V  V /  | || |\\  |\n  |_| \\___/ \\___/     \\_/\\_/  |___|_| \\_|\n");
+//        System.out.println(" _     ___  ____  _____ \n| |   / _ \\/ ___|| ____|\n| |  | | | \\___ \\|  _|  \n| |__| |_| |___) | |___ \n|_____\\___/|____/|_____|\n");
+//        welcome();
         view.printGamePlan();
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
-            int input = scanner.nextInt();
-            view.inputArrow(input);
+            char input  = scanner.next().charAt(0);
+            System.out.println(input);
+            int arrow = handle(input);
+//            int input = scanner.nextInt();
+//            view.inputArrow(input);
+            view.inputArrow(arrow);
             view.printGamePlan();
         }
 
--- src/com/company/View.java
+++ src/com/company/View.java
@@ -1,5 +1,7 @@
 package com.company;//package game;
 
+import java.io.IOException;
+
 public class View {
 
 
@@ -14,21 +16,22 @@ public class View {
 
 
     //  clean Previous map if using windows os using cls else (using mac os or linux system using "^c".
-//    private void clear() throws IOException, InterruptedException {
-//
-//        final String operatingSystem = System.getProperty("os.name");
-//        if (System.getProperty("os.name").contains("Windows"))
-//        {
-//            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-//        }
-//        else
-//        {
-//            System.out.print("\033\143");
-//        }
-//    }
+    private void clear() throws IOException, InterruptedException {
+
+        final String operatingSystem = System.getProperty("os.name");
+        if (System.getProperty("os.name").contains("Windows"))
+        {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        }
+        else
+        {
+            System.out.print("\033\143");
+        }
+    }
 
     public void startGame(){
-
+        String welcome = "\n\n\nWelcome to 2048 Game ;) \nChoose the Arrow to move on blocks !\nYou can use [w for up, a for left, d for right, s for down .";
+        System.out.println(welcome);
         //  create game with makeGame function . when game make function called create game array with zero value and randomize select 2 position and values of these set to  2 or 4 value
         ctrlObject.makeGame();
 
@@ -36,16 +39,31 @@ public class View {
 //        gameArray = ctrlObject.getGameArray();
 
     }
-    public void printGamePlan(){
+    public void printGamePlan() throws IOException, InterruptedException {
+//        for (int i = 0;i < 10;i++)
+//        {
+//            System.out.println("\b");
+//        }
+//        clear(); //اگر ارور داشت مشکل از عدم سازگاری ترمینال با این تابع هستش
         int[][] gameArray = ctrlObject.getGameArray();
+
+//        for(int i = 0;i < sizeArray;i++)
+//        {
+        System.out.println("\n========================");
+        System.out.println("\n|-----+-----+-----+-----|");
+//        }
         for (int i = 0;i < sizeArray;i++)
         {
             for(int k = 0;k < sizeArray;k++)
             {
-                System.out.print(gameArray[i][k] + "\t");
+                System.out.print("|  "+gameArray[i][k] + "  ");
             }
-            System.out.println();
+            System.out.println("|\n|-----+-----+-----+-----|");
+
+//            System.out.println("|");
         }
+        System.out.println("\n\n=========================\n");
+        System.out.println("input arrow you want.\nYou can input w, a, s, d : ");
     }
     public void inputArrow(int arrow){
         if(ctrlObject.isLose()){
@@ -61,14 +79,17 @@ public class View {
     }
     private void Win(){
         if(ctrlObject.isWin()) {
-            System.out.println("win");
+//            System.out.println("win");
+            System.out.println("__   _____  _   _  __        _____ _   _ \n\\ \\ / / / / _ \\ \\| | | |    / /|_ _|\\ \\ |\n \\ V / | | | | | |  \\ \\ /\\ / / | ||  \\| |\n  | || |_| | |_| |   \\ V  V /  | || |\\  |\n  |_| \\___/ \\___/     \\_/\\_/  |___|_| \\_|\n");
             System.exit(0);
+
 
         }
     }
     private void Lose(){
         if (ctrlObject.isLose()) {
-            System.out.println("Loss");
+//            System.out.println("Loss");
+            System.out.println(" _     ___  ____  _____ \n| |   / _ \\/ ___|| ____|\n| |  | | | \\___ \\|  _|  \n| |__| |_| |___) | |___ \n|_____\\___/|____/|_____|\n");
             System.exit(0);
 
         }
